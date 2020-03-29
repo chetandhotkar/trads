30f6a7c72b23a871dd5b7e8ad5f6f788b96f4012
chetandhotkar <37110363+chetandhotkar@users.noreply.github.com>
2020-03-29 17:52:44 +0530

    Update db_file.sql

--- db_file.sql
+++ db_file.sql
@@ -5,6 +5,7 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+
 DROP TABLE IF EXISTS `master_categories`;
 CREATE TABLE `master_categories` (
   `iCategoriesId` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incremented ',
